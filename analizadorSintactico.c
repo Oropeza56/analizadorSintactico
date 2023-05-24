8c867a8329a3392819041e8414aafeeec66bc093
Ana Fragoso <78662431+anafragoso2@users.noreply.github.com>
2023-05-23 19:50:03 -0600

    Update analizadorSintactico.c

--- analizadorSintactico.c
+++ analizadorSintactico.c
@@ -74,246 +74,91 @@ void siguiente(){
     ubicacion++;
 }
 
-void Program(){
-	//           ent                real				simb			large		  
-if(actual->info=='e'||actual->info=='r'||actual->info=='b'||actual->info=='l' ||actual->info =='['){
-        Decl();
-        if (actual->info == '[') {
-        	siguiente();
-        }else{
-         fprintf(erroresSintacticos,"ERROR EN (%d) falta '['\n",ubicacion);
-        }
-        Func();
-        otraFunc();
-	         if (actual->info == ']') {
-        	siguiente();
-        }else{
-         fprintf(erroresSintacticos,"ERROR EN (%d) falta ']'\n",ubicacion);
-        }
-    } else {
-        fprintf(erroresSintacticos,"ERROR EN (%d) al declarar una funcion\n",ubicacion);
-        return;
-    }
 
-}
-
-void otraFuncion(){
-	if (actual == NULL){
+	
+void T(){
+	if(actual -> info =='(' ||actual -> info == 'a' || actual -> info == 'n' ||actual -> info == 'x' || actual -> info == '['){
+		F();
+		TP();
 		return;
-	}else if(actual->info=='e'||actual->info=='r'||actual->info=='b'||actual->info=='l'){
-	Func();
-	otraFunc();	
-	}else{
-	 fprintf(erroresSintacticos, "ERROR EN (%d) al declarar una función o el fin archivo\n", ubicacion);	
+	}
+	else{
+		fprintf(erroresSintacticos, "ERROR EN (%d). Se esperaba '(', identificador, constante numérica entera, constante numérica real o '['\n", ubicacion);
+		return;
+		}
 	}
 
-}
 
-// funcion -> tipo + a + ( + param + ) + { + cuerpo + }
-void Func(){
-	if(actual->info=='e'||actual->info=='r'||actual->info=='b'||actual->info=='l'){
-		Tipo();
-		  if(actual->info == 'a'){
-		  	siguiente();
-		  }else{
-		  	fprintf(erroresSintacticos, "ERROR EN (%d) falta el identificador \n", ubicacion );
-		  	return;
-		  }
-		  if(actual->info == '('){
-		  	siguiente();
-		  }else{
-		  fprintf(erroresSintacticos, "ERROR EN (%d) falta '('\n", ubicacion);
-		 return;
-		 }
-
-		 Param();
-
-		 if(actual->info == ')'){
-		  	siguiente();
-		  }else{
-		  fprintf(erroresSintacticos, "ERROR EN (%d) falta ')'\n", ubicacion);
-		 return;
-		 }
-
-		 if(actual->info == '{'){
-		  	siguiente();
-		  }else{
-		  fprintf(erroresSintacticos, "ERROR EN (%d) falta '{'\n", ubicacion);
-		 return;
-		 }
-
-		 Cuerpo();
-
-		 if(actual->info == '}'){
-		  	siguiente();
-		  }else{
-		  fprintf(erroresSintacticos, "ERROR EN (%d) falta '}'\n", ubicacion);
-		 return;
-		 }
-
-	} else{
-		fprintf(erroresSintacticos, "ERROR EN (%d) falta el tipo de dato \n", ubicacion);
-	}
-}
-
-void Param(){
-	if(actual->info=='e'||actual->info=='r'||actual->info=='b'||actual->info=='l'){
-		Tipo();
-		if(actual->info == 'a'){
+	void F(){
+		if(actual -> info == 'a'|| actual -> info == 'n'|| actual -> info == 'x'){
 			siguiente();
-		}else{
-			fprintf(erroresSintacticos, "ERROR EN (%d) falta el identificador\n", ubicacion );
 			return;
-		}
-		otroParam();
-	}else if (actual->info == ')'){
-		return;
-	}else{
-		fprintf(erroresSintacticos, "ERROR EN (%d) falta el tipo de dato del parametro\n", ubicacion);
-	}
-}
-
-void otroParam(){
-	if (actual->info == ','){
-		siguiente();
-		Tipo();
-		if (actual->info=='a'){
+		}else if (actual -> info == '['){
+			Llama();
+		}else if(actual -> info == '('){
 			siguiente();
-		}else{
-			fprintf(erroresSintacticos, "ERROR EN (%d) falta el identificador\n", );
+			E();
+			if(actual -> info == ')'){
+				siguiente();
+			}
+			else{
+				fprintf(erroresSintacticos, "ERROR EN (%d). Se esperaba ')'", ubicacion);
+			}
 			return;
+			else{
+				fprintf(erroresSintacticos, "ERROR EN (%d). Se esperaba '(', '[', identificador, constante númerica entera o 
+				constante númerica real\n", ubicacion);
+			}
 		}
-		otroParam();
-
-	}else if (actual->info== ')'){
-		return;
-	}else{
-		fprintf(erroresSintacticos, "ERROR EN (%d) falta el tipo de dato del parametro\n", ubicacion );
 	}
-}
-
-void Cuerpo(){
-	if(actual->info=='e'||actual->info=='r'||actual->info=='b'||actual->info=='l'||actual->info=='a'||actual->info=='i'||actual->info=='h'||actual->info=='m'||actual->info=='t'||actual->info=='['||actual->info=='v'||actual->info=='s'||actual->info=='}'){
-		Decl();
-		SP();
-	}else{
-		fprintf(erroresSintacticos, "ERROR EN (%d) falta cuerpo de funcion, '}' ó '['\n", ubicacion );
-	}
-
-}
-
-void Decl(){
-	if(actual->info=='['||actual->info=='a'||actual->info=='i'||actual->info=='h'||actual->info=='m'||actual->info=='t'||actual->info=='v'||actual->info=='s'||actual->info=='}'||){
-		return;
-	}else if(actual->info=='e'||actual->info=='r'||actual->info=='b'||actual->info=='l'){
-		D();
-		Decl();
-	}else{
-		fprintf(erroresSintacticos, "ERROR EN (%d) falta la declaración\n", ubicacion );
-	}
-}
-
-void D(){
-	if(actual->info=='e'||actual->info=='r'||actual->info=='b'||actual->info=='l'){
-		Tiṕo();
-		if (actual->info=='a'){
+					
+					
+	void RP(){
+		if(actual -> info == '>'|| actual -> info == '<'|| actual -> info == 'q'|| actual -> info == 'g'||actual -> info == 'd' || actual -> info == 'p' ){
 			siguiente();
-		}else{
-			fprintf(erroresSintacticos, "ERROR EN (%d) falta el identificador \n", ubicacion);
 			return;
-		}
-		v();
-		DP();
-	}else{
-		fprintf(erroresSintacticos, "ERROR EN (%d) falta el tipo de dato \n", ubicacion);
-	}
-}
-
-void Tipo(){
-	if(actual->info=='e'||actual->info=='r'||actual->info=='b'||actual->info=='l'){{
-		siguiente();
-	}
-	fprintf(erroresSintacticos, "ERROR EN (%d) falta el tipo de dato\n", ubicacion);
-}
-
-void DP(){
-	if (actual->info == ';'){
-		siguiente();
-		return:
-	}else if (actual->info == ','){
-	   siguiente();
-	   if (actual->info== 'a'){
-	   	 siguiente();
-	   }else{
-	   	fprintf(erroresSintacticos, "ERROR EN (%d) falta el identificador\n", ubicacion);
-	   }
-	   V();
-	   DP();
-	   return;
-	}else{
-		fprintf(erroresSintacticos, "ERROR EN (%d) falta ',' ó ';' \n", ubicacion);
-	}
-}
-
-void V(){
-	if (actual->info == '='){
-		siguiente();
-	    C();
-	    return;
-	}else{
-		fprintf(erroresSintacticos, "ERROR EN (%d) falta '='\n", ubicacion);
-		return;
-	}
-}
-
-void C(){
-	if(actual->info=='n'||actual->info=='c'||actual->info=='x'){
-		siguiente();
-		return;
-	}else{
-		fprintf(erroresSintacticos, "ERROR EN (%d) falta una constante númerica entera, cadena ó caracter\n", ubicacion);
-	}
-}
-
-void A(){
-	if (actual->info=='a'){
-	   siguiente();
-	   if (actual-> info =='=') {
-	   	siguiente();
-	   }else{
-	   	fprintf(erroresSintacticos, "ERROR EN (%d) falta '='\n", ubicacion)
-	   }
-	   AP();
-	   if (actual->info==';')  {
-	   	siguiente();
-	   }else{
-	   	fprintf(erroresSintacticos, "ERROR EN (%d) falta ';'\n", ubicacion );
-	   }
-	}else{
-	   	fprintf(erroresSintacticos, "ERROR EN (%d) falta el identificador\n", ubicacion );
-	   	return;
-	}
-}
-
-void AP(){
-	if (actual->info=='c'){
-		 siguiente();
-		 return;
-		}else if (actual->info== '('|| actual->info == 'a'|| actual->info == 'n'|| actual->info == 'x'|| actual->info == '[' ){
-		 	E();
-		 	return	
 		}else{
-			fprintf(erroresSintacticos, "ERROR EN (%d) falta '(', identificador, constante númerica entera, constante númerica real, constante númerica real ó '[' \n", ubicacion);
-			return;
+			fprintf(erroresSintacticos, "ERROR EN (%d). Se esperaba un operador relacional", ubicacion);
 		}
-}
-
-void E(){
-	if (actual->info== '('|| actual->info == 'a'|| actual->info == 'n'|| actual->info == 'x'|| actual->info == '[' ){
-		T();
-		EP();
-		return;	
 	}
-}
+					
+	void S(){
+		if(actual -> info == 'a'){
+			A();
+			return;
+		}else if(actual -> info == 'i'){
+			B();
+			return;
+		}else if(actual -> info == 'h'){
+			H();
+			return;
+		}else if(actual -> info == 'm'){
+			M();
+			return;
+		}else if(actual -> info == 't'){
+			P();
+			return;
+		}else if(actual -> info == '['){
+			Llama();
+			return;
+		}else if(actual -> info == 'v'){
+			Devuelve();
+			return;
+		}else if(actual -> info == 's'){
+			siguiente();
+			if(actual -> info == ';'){
+				siguiente();
+			}else{
+				fprintf(erroresSintacticos, "ERROR EN (%d). Se esperaba ';'", ubicacion);
+			}
+			return;
+		}else{
+			fprintf(erroresSintacticos, "ERROR EN (%d). Se esperaba un identificador, o la palabra reservada 'si', o la palabra reservada 'haz', 
+			o la palabra reservada 'mientras', o la palabra reservada 'para', o la palabra reservada 'regresa', o una constante cadena, o '['"
+			,ubicacion);
+		}
+	}
+					
+					
 
 
